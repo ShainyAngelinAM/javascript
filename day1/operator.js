9854dbc541811a7c93553d8b5b998a446ed424bc
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-03 10:51:02 +0530

    Add files via upload

---
+++ day1/operator.js
@@ -0,0 +1,56 @@
+let a=2;
+let b=3;
+
+//arithmetic
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//comparison
+console.log(a>b);
+console.log(a>=b);
+console.log(a<b);
+console.log(a<=b);
+
+//logical
+console.log(a>5 && b<5);
+console.log(a>5 || b>5);
+console.log(!(a>5));
+
+//assignment
+let c=5;
+c+=3;
+console.log(c);
+
+//string
+let fname="shainy";
+let course="js";
+console.log(fname  ,  course);
+
+//ternary
+let age=18;
+let vote=(age>=18)?'yes':'no';
+console.log(vote);
+
+//increment
+let count =5;
+console.log(count++);
+console.log(count--);
+console.log(count);
+console.log(--count);
+console.log(++count);
+console.log(count);
+
+//bitwise
+let value1=4;
+let value2=5;
+console.log(value1&value2);
+console.log(value1|value2);
+console.log(value1^value2);
+console.log(~value1);
+console.log(value1<<value2);
+console.log(value1>>value2);
+console.log(value1>>value2);
