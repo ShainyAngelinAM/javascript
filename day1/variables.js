25d9effb4e82beb2785bec79b22c5c78f413e718
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-03 10:15:39 +0530

    Add files via upload

---
+++ day1/variables.js
@@ -0,0 +1,27 @@
+//var
+var a=1;
+if(a){
+    var a=2;
+    console.log(a);
+}
+console.log(a);
+
+//let
+let b=1;
+if(b){
+    let b=2;
+    console.log(b);
+}
+console.log(b);
+if(b){
+    let b=3;
+    console.log(b);
+}
+console.log(b);
+
+//const
+const c=10;
+if(c){
+    c==20;
+    console.log(c);
+}
