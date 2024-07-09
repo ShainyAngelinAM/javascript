74c8c23b77b65ad34f049c25b6ffc9fd79c8df94
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-09 22:58:44 +0530

    Add files via upload

---
+++ day6/src/mount.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Mounting from "./components/mounting";
+export class mount extends Component {
+  render() {
+    return (
+        <div classname="app">
+        <Mounting/>
+        </div>
+    )
+  }
+}
+
+export default mount;
