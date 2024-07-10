ee63200cda86b356ecb2eb66d4c9be629fbfea9a
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-10 17:29:21 +0530

    Add files via upload

---
+++ day8/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Event from './components/onPaste'
+import First from './components/submit'
+import Ifelse from './components/ifelse'
+import Ternary from './components/ternary'
+function App() {
+
+  return (
+    <div className="App">
+      <Event/>
+      <First/>
+      <Ifelse/>
+      <Ternary/>s
+    </div>
+  );
+}
+
+export default App;
