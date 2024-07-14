34c29fc86992563723ce24f1a47d6deae66e0c29
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-14 21:48:33 +0530

    Add files via upload

---
+++ day10/src/Refhook.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Ref from './useref/ref';
+import Timer from './useref/timer';
+function Apply(){
+    return(
+        <div>
+            <Ref/><br></br><br></br>
+            <Timer/><br></br><br></br>
+        </div>
+    )
+}
+export default Apply
