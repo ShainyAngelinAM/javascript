3f7fa667a14eda4db3b6ae9e179eb286c8e9e019
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-29 00:11:47 +0530

    Add files via upload

---
+++ Assignment/Leave Management System/backend/config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'shainy',
+    database: 'Leave Management System',
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+};
