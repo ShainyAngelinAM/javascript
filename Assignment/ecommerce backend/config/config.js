eea77f90abc686c4334e879e243312937b55baad
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-28 22:38:19 +0530

    Add files via upload

---
+++ Assignment/ecommerce backend/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    development: {
+        username: 'postgres',
+        password: 'shainy',
+        database: 'ECOMMERCE',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    test: {
+        username: 'postgres',
+        password: 'shainy',
+        database: 'ECOMMERCE',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+      },
+    production: {
+        username: 'postgres',
+        password: 'shainy',
+        database: 'ECOMMERCE',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    }
+};
