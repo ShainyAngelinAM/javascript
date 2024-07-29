7eec32caac5ebae61f782334e87e4f7fbb987562
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-29 10:37:13 +0530

    Add files via upload

---
+++ Assignment/Leave Management System/backend/utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+async function sendEmail(to, subject, text) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: 'angelin0107vs@gmail.com', 
+      pass: 'idyb olib xtry beoi', 
+    },
+  });
+
+  let mailOptions = {
+    from: 'angelin0107vs@gmail.com', 
+    to, 
+    subject, 
+    text, 
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return reject(error); 
+      }
+      resolve(info); 
+    });
+  });
+}
+
+module.exports = sendEmail;
