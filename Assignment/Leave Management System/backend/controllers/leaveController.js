75748c657c120446e980c103b85b40177ca36a29
ShainyAngelinAM <131961509+ShainyAngelinAM@users.noreply.github.com>
2024-07-29 10:38:10 +0530

    Add files via upload

--- Assignment/Leave Management System/backend/controllers/leaveController.js
+++ Assignment/Leave Management System/backend/controllers/leaveController.js
@@ -1,5 +1,7 @@
-const db  = require('../models');
-const dbleave = db.LeaveRequest
+const sendEmail = require('../utils/sendEmail');
+const db = require('../models');
+const dbleave = db.LeaveRequest;
+const dbeUser = db.User; 
 const { Op } = require('sequelize');
 
 exports.createLeaveRequest = async (req, res) => {
@@ -23,37 +25,48 @@ try {
   res.status(500).json({ message: 'Internal server error' });
 }};
 
+
 exports.approveLeaveRequest = async (req, res) => {
   const { id } = req.params;
   try {
-    const [updatedCount, updatedRequests] = await dbleave.update(
-      { status: 'approved' }, 
-      { where: { id }, returning: true } 
-    );
-    if (updatedCount === 0) {
+    const leaveRequest = await dbleave.findOne({ where: { id } });
+    if (!leaveRequest) {
       return res.status(404).json({ message: 'Leave request not found' });
     }
-    return res.status(200).json(updatedRequests[0]);
+    leaveRequest.status = 'approved';
+    await leaveRequest.save();
+    const user = await dbeUser.findOne({ where: { username: leaveRequest.username } });
+    if (user) {
+      await sendEmail(user.email, 'Leave Request Approved', `Your leave request has been approved.`);
+    }
+
+    return res.status(200).json(leaveRequest);
   } catch (error) {
     console.error('Error approving leave request:', error);
     return res.status(500).json({ message: 'Internal server error' });
-  }};
+  }
+};
 
 exports.denyLeaveRequest = async (req, res) => {
   const { id } = req.params;
   try {
-    const denied = await dbleave.update(
-      { status: 'denied' }, 
-      { where: { id } } 
-    );
-    if (!denied[0]) {
+    const leaveRequest = await dbleave.findOne({ where: { id } });
+    if (!leaveRequest) {
       return res.status(404).json({ message: 'Leave request not found' });
     }
+    leaveRequest.status = 'denied';
+    await leaveRequest.save();
+    const user = await dbeUser.findOne({ where: { username: leaveRequest.username } });
+    if (user) {
+      await sendEmail(user.email, 'Leave Request Denied', `Your leave request has been denied.`);
+    }
+
     return res.status(200).json({ message: 'Leave request denied successfully' });
   } catch (error) {
     console.error('Error denying leave request:', error);
     return res.status(500).json({ message: 'Error denying leave request' });
-  } };
+  }
+};
 
 exports.getLeaveHistory = async (req, res) => {
   try {
@@ -99,3 +112,6 @@ exports.getUserLeaveHistory = async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+
+
